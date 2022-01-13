2194ae2bc5f6fb6ffc8aee00251e20c4453d542b
Dev-BaeJS <aqowjdtna@ajou.ac.kr>
2022-01-13 23:59:44 +0900

    22.01.13 openCV ch02-1

---
+++ ch02/img_info.py
@@ -0,0 +1,69 @@
+﻿import sys
+import cv2
+
+
+# 영상 불러오기
+img1 = cv2.imread('cat.bmp', cv2.IMREAD_GRAYSCALE)
+img2 = cv2.imread('cat.bmp', cv2.IMREAD_COLOR)
+
+if img1 is None or img2 is None:
+    print('Image load failed!')
+    sys.exit()
+
+# 영상의 속성 참조
+print('type(img1):', type(img1))
+print('img1.shape:', img1.shape) # 2차원 (480 세로, 640 가로)
+print('img2.shape:', img2.shape) # 3차원
+print('img1.dtype:', img1.dtype) # uint8
+print('img1.dtype:', img2.dtype) # uint8
+
+
+# 영상의 크기 참조
+h, w = img1.shape
+print('w x h = {} x {}'.format(w, h))
+
+# h, w = img2.shape # 3차원인데 2개만 받으려고 하니 오류가 남
+# print('w x h = {} x {}'.format(w, h))
+
+h, w = img2.shape[:2]
+print('img2 size: {} x {}'.format(w, h))
+
+# grayscale 인지 판단
+# if img1.ndim == 2: # if len(img1.shape) == 2:
+#     print('img1 is a graysclae image')
+    
+if len(img1.shape) == 2:
+    print('img1 is a grayscale image')
+elif len(img1.shape) == 3:
+    print('img1 is a truecolor image')
+
+cv2.imshow('img1', img1)
+cv2.imshow('img2', img2)
+cv2.waitKey()
+
+# 영상의 픽셀 값 참조
+# x = 20 y = 10의 픽셀 값 가져옴
+x = 20
+y = 10
+p1 = img1[y,x]
+print(p1)
+p2 = img2[y,x]
+print(p2)
+
+# img1의 모든 픽셀을 그레이스케일 255로 바꿈
+# img2의 모든 픽셀을 r = 255로 바꿈
+# @@이 방식은 매우 느리기 때문에 실제로는 사용하면 안됨@@
+for y in range(h):
+    for x in range(w):
+        img1[y, x] = 255
+        img2[y, x] = (0, 0, 255)        
+
+# 이 방식이 훨씬 빠르게 동작함
+# img1[:,:] = 255
+# img2[:,:] = (0, 0, 255)
+
+cv2.imshow('img1', img1)
+cv2.imshow('img2', img2)
+cv2.waitKey()
+
+cv2.destroyAllWindows()
---
+++ ch02/img_ops.py
@@ -0,0 +1,49 @@
+﻿import numpy as np
+import cv2
+
+
+# 새 영상 생성하기
+img1 = np.empty((240, 320), dtype=np.uint8)       # grayscale image // 쓰레기값을 집어넣음
+img2 = np.zeros((240, 320, 3), dtype=np.uint8)    # color image // 모든 픽셀을 0으로
+img3 = np.ones((240, 320), dtype=np.uint8) * 255  # dark gray // 모든 픽셀을 1*255로
+img4 = np.full((240, 320, 3), (0, 255, 255), dtype=np.uint8)  # yellow // 모든 픽셀을 
+
+cv2.imshow('img1', img1)
+cv2.imshow('img2', img2)
+cv2.imshow('img3', img3)
+cv2.imshow('img4', img4)
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+# 영상 복사
+img1 = cv2.imread('HappyFish.jpg')
+
+img2 = img1 # img1 의 주소값을 복사하기 때문에 img1을 수정해도 img2가 수정된다 참조의 개념
+img3 = img1.copy() # 메모리를 새로 할당하기 때문에 img1이 수정되어도 영향이 없다
+
+#img1.fill(255)
+
+cv2.imshow('img1', img1)
+cv2.imshow('img2', img2)
+cv2.imshow('img3', img3)
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+# 부분 영상 추출
+img1 = cv2.imread('HappyFish.jpg')
+
+# 40번째 행부터 120번째 행까지 , 30번째 열부터 150번째 열까지
+img2 = img1[40:120, 30:150]  # numpy.ndarray의 슬라이싱
+img3 = img1[40:120, 30:150].copy()
+
+img1[:, :] = (0, 255, 255) # img1의 모든 픽셀을 노란색으로 변경하면 img2도 영향받음
+
+# img2.fill(0)
+# img2의 50,50 좌표를 기준으로 반지름이 20이고 색은 빨간색이고 두께는 2의 원을 그려라
+cv2.circle(img2, (50,50), 20, (0,0,255), 2)
+
+cv2.imshow('img1', img1)
+cv2.imshow('img2', img2)
+cv2.imshow('img3', img3)
+cv2.waitKey()
+cv2.destroyAllWindows()
